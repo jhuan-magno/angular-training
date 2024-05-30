bf1d303b5cb0b87da16174ed5661cee6febf7e81
Jhuan Magno <jhuan.dev@gmail.com>
2024-05-30 12:42:00 -0400

    criar formulário de inserção de pensamentos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { TitleAndParagraphComponent } from './components/title-and-paragraph/title-and-paragraph.component';
 import { CabecalhoComponent } from './components/cabecalho/cabecalho.component';
 import { RodapeComponent } from './components/rodape/rodape.component';
+import { CriarPensamentosComponent } from './components/pensamentos/criar-pensamentos/criar-pensamentos.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TitleAndParagraphComponent,
     CabecalhoComponent,
-    RodapeComponent
+    RodapeComponent,
+    CriarPensamentosComponent
   ],
   imports: [
     BrowserModule,
