30ebe40c97f8ba566b89667def628cd642f0c6a1
Jhuan Magno <jhuan.dev@gmail.com>
2024-05-30 18:27:47 -0400

    Adicionar rotas nos botoes de criar pensamento e cancelar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CriarPensamentosComponent } from './components/pensamentos/criar-pensamentos/criar-pensamentos.component';
+import { ListarPensamentosComponent } from './components/pensamentos/listar-pensamentos/listar-pensamentos.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'',
+    redirectTo: 'listarPensamento',
+    pathMatch:'full'
+  },
+  {
+    path:'criarPensamentos',
+    component: CriarPensamentosComponent
+  },
+  {
+    path:'listarPensamentos',
+    component: ListarPensamentosComponent
+  },
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
